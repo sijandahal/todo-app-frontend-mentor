e393e8ad42a65cf78af34765bf16f14b0d10a1c6
Sijan Dahal <47341534+sijandahal@users.noreply.github.com>
2021-01-07 07:48:44 +0545

    added functionalities

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 var body = document.body;
-const form = document.querySelector('.form');
-let value = document.querySelector('.inputtext');
-const collection = document.querySelector('.collection');
-const items = document.querySelector('.items');
-const darkimage = document.querySelector('#darkimage');
-const link = document.querySelector('.link');
-const checkbox = document.querySelectorAll('.checkbox');
-const listItems = document.querySelectorAll('.list-collection');
-const completedButton = document.querySelector('.completed');
-const activeButton = document.querySelector('.activee');
-const all = document.querySelector('.all');
-const clearButton = document.querySelector('.clearCompleted');
-
-//list item childrens
-
-const listedItems = collection.childNodes;
-let li = document.querySelectorAll('.list-collection');
-
+const form = document.querySelector(".form");
+let value = document.querySelector(".inputtext");
+const collection = document.querySelector(".collection");
+const items = document.querySelector(".items");
+const darkimage = document.querySelector("#darkimage");
+const link = document.querySelector(".link");
+const checkbox = document.querySelectorAll(".checkbox");
+const listItems = document.querySelectorAll(".list-collection");
+const completedButton = document.querySelector(".completed");
+const activeButton = document.querySelector(".activee");
+const all = document.querySelector(".all");
+const ClearButton = document.querySelector(".clearCompleted");
 
 //event listeners
-form.addEventListener('submit', formSubmit);
-darkimage.addEventListener('click', changeImage);
-collection.addEventListener('click', chageList);
-completedButton.addEventListener('click', completedTasks);
-activeButton.addEventListener('click', activeList);
-all.addEventListener('click', allListItems);
-clearButton.addEventListener('click', clearCompleted);
+form.addEventListener("submit", formSubmit);
+darkimage.addEventListener("click", changeImage);
+collection.addEventListener("click", chageList);
+completedButton.addEventListener("click", completedTasks);
+activeButton.addEventListener("click", activeList);
+all.addEventListener("click", allListItems);
+ClearButton.addEventListener("click", clearButton);
 
+
+//function getting dynamic count
+
+function elementsCount() {
+   //getting the child elements from the collection
+   collection.childElementCount;
+   let count = collection.childElementCount;
+   items.textContent = count;
+   form.reset();
+}
 //formSubmit
 function formSubmit(e) {
   const inputValue = value.value;
-   if (inputValue === '') {
-     alert('Please enter a value')
-   }
-   
-   else {
+  if (inputValue === "") {
+    alert("Please enter a value");
+  } else {
     collection.innerHTML += `
     <li class = "list-collection">
     <label class="checkbox" id = "label">
@@ -48,91 +50,92 @@ function formSubmit(e) {
     ${inputValue}
     <a href="#">
     <img src="./images/icon-cross.svg" alt="Cross" class = "delete"> </a>
-  </li>`
-   }
-    //getting the child elements from the collection
-
-   collection.childElementCount
-  let count =  collection.childElementCount;
-   items.textContent = count;
-   form.reset();
+  </li>`;
+  }
+   elementsCount();
   e.preventDefault();
-  
 }
 
 //imagechange
 var toggle = false;
 function changeImage() {
-    if (toggle === true) {
-       darkimage.src  = './images/icon-sun.svg';
-    } else {
-      darkimage.src = '/images/icon-moon.svg';
-    }
-    toggle = !toggle; 
-    body.classList.toggle("toggle");
+  if (toggle === true) {
+    darkimage.src = "./images/icon-sun.svg";
+  } else {
+    darkimage.src = "/images/icon-moon.svg";
+  }
+  toggle = !toggle;
+  body.classList.toggle("toggle");
 }
 
 //delete li from the collection
 function chageList(e) {
-   if (e.target.classList.contains('delete')) {
-     e.target.parentElement.parentElement.remove();
-   }
-    
-   //for checkmark
-   if (e.target.classList.contains('checkbox')) {
-     const li = e.target.parentElement;
-     li.classList.toggle('line-through')
-   }
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.parentElement.remove();
+  }
+
+  //for checkmark
+  if (e.target.classList.contains("checkbox")) {
+    const li = e.target.parentElement;
+    li.classList.toggle("line-through");
+  }
+  elementsCount();
 }
 
 //completed button
 
 function completedTasks(e) {
-  li.forEach(function(item) {
-   if  (item.classList.contains('line-through')) {
-     item.style.display = "flex";
-     console.log("hello");
-   }
-   else {
-     item.style.display = "none"
-   }
-   })
-   e.preventDefault();
+  const listedItems = collection.childNodes;
+  let li = document.querySelectorAll(".list-collection");
+  li.forEach(function (item) {
+    if (item.classList.contains("line-through")) {
+      item.style.display = "flex";
+      console.log("hello");
+    } else {
+      item.style.display = "none";
+    }
+  });
+  e.preventDefault();
+  elementsCount();
 }
 //active button
 function activeList(e) {
-  li.forEach(function(item) {
-   if  (!item.classList.contains('line-through')) {
-     item.style.display = "flex";
-   }
-   else {
-     item.style.display = "none"
-   }
-   });
-   e.preventDefault();
-  };
+  const listedItems = collection.childNodes;
+  let li = document.querySelectorAll(".list-collection");
+  li.forEach(function (item) {
+    if (!item.classList.contains("line-through")) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+  e.preventDefault();
+  elementsCount();
+}
 
 //allitems
 
 function allListItems(e) {
-  li.forEach(function(item) {
-    if  (item.classList.contains('list-collection')) {
+  const listedItems = collection.childNodes;
+  let li = document.querySelectorAll(".list-collection");
+  li.forEach(function (item) {
+    if (item.classList.contains("line-through")) {
       item.style.display = "flex";
       console.log("hello");
     }
-    })
-    e.preventDefault();
-  }
-
-//clearButton
-
-function clearCompleted(e) {
-  li.forEach(function(item) {
-    if  (item.classList.contains('line-through')) {
-    item.style.display = 'none';
-    }
-   e.preventDefault(e);
-})
+  });
+  e.preventDefault();
+  elementsCount();
 }
 
-
+function clearButton(e) {
+  const listedItems = collection.childNodes;
+  let li = document.querySelectorAll(".list-collection");
+  li.forEach(function (item) {
+    if (item.classList.contains("line-through")) {
+      item.style.display = "none";
+    }
+  });
+  e.preventDefault();
+  elementsCount();
+}
